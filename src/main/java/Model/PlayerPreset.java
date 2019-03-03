4db75a094cd8f743bed5bf75f329fe0ddd7abe8e
Temirkhan <temirkhan.nasukhov@gmail.com>
2019-03-03 16:23:21 +0300

    Created player preset

---
+++ src/main/java/Model/PlayerPreset.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class PlayerPreset {
+    private String name;
+
+    PlayerPreset(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/Model/PlayerPresetTest.java
@@ -0,0 +1,14 @@
+package Model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerPresetTest {
+    private final PlayerPreset playerPreset = new PlayerPreset("Some name");
+
+    @Test
+    public void name() {
+        assertEquals("Some name", playerPreset.getName());
+    }
+}
