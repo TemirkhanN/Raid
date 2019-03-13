1152ca076567eaeb7ba9813075e22703b27b8c13
Temirkhan <temirkhan.nasukhov@gmail.com>
2019-03-13 03:33:59 +0300

    Overseer has been singletoned

--- src/main/java/Overseer/Overseer.java
+++ src/main/java/Overseer/Overseer.java
@@ -1,8 +1,32 @@
 package Overseer;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Overseer {
+    private static Overseer instance;
+
+    private Set<Subject> subjects = new HashSet<Subject>();
+
+    public static Overseer getInstance() {
+        if (instance == null) {
+            instance = new Overseer();
+        }
+
+        return instance;
+    }
+
     public void observe(Subject subject) {
+        if (subjects.contains(subject)) {
+            return;
+        }
+
+        // TODO Some observation strategies iduhno
+        subjects.add(subject);
+    }
+
+    private Overseer() {
 
     }
 }
--- src/main/java/TemirkhanN/Main.java
+++ src/main/java/TemirkhanN/Main.java
@@ -1,19 +1,10 @@
 package TemirkhanN;
 
+import Boss.Boss;
+import Boss.Raid;
+import Overseer.Overseer;
 import Player.*;
 import Player.Exception.*;
-
-/**
-
-interface Raid extends Subject {
-    public Raid createRaid(Boss boss, Party party);
-
-    public Boss getBoss();
-
-    public Party getParty();
-}
-
-*/
 
 public class Main {
 
@@ -25,15 +16,19 @@ public class Main {
         // Create another player
         PlayerPreset anotherPreset = new PlayerPreset("Gilbert", 90);
         Player anotherPlayer = new Player(anotherPreset);
-        // Some player teams up with another player and create party
         try {
-            somePlayer.partyUp(anotherPlayer);
+            // Some player teams up with another player and create party
+            Party braveParty = somePlayer.partyUp(anotherPlayer);
+            // Find some boss
+            Boss raidBoss = new Boss("Beorn");
+            // Instantiate raid with found boss and created party
+            Raid raid = new Raid(raidBoss, braveParty);
+            // Pass raid to game observer and instantiate calculations with callbacks, timeouts and etc
+            Overseer gameMaster = Overseer.getInstance();
+            gameMaster.observe(raid);
+            // Players takes some actions || Simultaneously boss takes some actions based on predefined primitive AI
         } catch (LogicException exception) {
             System.exit(126);
         }
-        // Find some boss
-        // Instantiate raid with found boss and created party
-        // Pass raid to game observer and instantiate calculations with callbacks, timeouts and etc
-        // Players takes some actions || Simultaneously boss takes some actions based on predefined primitive AI
     }
 }
