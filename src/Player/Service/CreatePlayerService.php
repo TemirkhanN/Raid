244309a15aa84d23927bce21c39de7f6375aebe7
Temirkhan <temirkhan.nasukhov@gmail.com>
2019-03-31 20:11:47 +0300

    Fixed cs

--- src/Player/Service/CreatePlayerService.php
+++ src/Player/Service/CreatePlayerService.php
@@ -7,8 +7,18 @@ namespace Raid\Player\Service;
 use Raid\Player\ValueObject\PlayerPreset;
 use Raid\Player\Model\Player;
 
+/**
+ * Player creation service
+ */
 class CreatePlayerService
 {
+    /**
+     * Creates player
+     *
+     * @param PlayerPreset $playerPreset
+     *
+     * @return Player
+     */
     public function execute(PlayerPreset $playerPreset): Player
     {
         $player = new Player($playerPreset);
