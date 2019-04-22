52896e433663dda4495081b0f35292f6dbeb88c3
Temirkhan <diezza.com@gmail.com>
2019-04-22 12:08:29 +0300

    Codestyle fixes

--- src/Player/Repository/InMemoryPlayerRepository.php
+++ src/Player/Repository/InMemoryPlayerRepository.php
@@ -6,18 +6,35 @@ namespace Raid\Player\Repository;
 
 use Raid\Player\Model\Player;
 
+/**
+ * In-memory player repository
+ */
 class InMemoryPlayerRepository implements PlayerRepositoryInterface
 {
     /**
+     * Player list
+     *
      * @var \SplObjectStorage|Player[]
      */
     private $players;
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
         $this->players = new \SplObjectStorage();
     }
 
+    /**
+     * Saves player into repository
+     *
+     * @param Player $player
+     *
+     * @return void
+     *
+     * @throws \RuntimeException
+     */
     public function savePlayer(Player $player): void
     {
         if ($this->players->contains($player)) {
@@ -25,12 +42,19 @@ class InMemoryPlayerRepository implements PlayerRepositoryInterface
         }
 
         if ($this->findPlayerByName($player->getName())) {
-            throw new \RuntimeException(sprintf('Player with name "%" already exists', $player->getName()));
+            throw new \RuntimeException(sprintf('Player with name "%s" already exists', $player->getName()));
         }
 
         $this->players->attach($player);
     }
 
+    /**
+     * Finds player by name
+     *
+     * @param string $playerName
+     *
+     * @return Player|null
+     */
     public function findPlayerByName(string $playerName): ?Player
     {
         foreach ($this->players as $player) {
--- src/Player/Repository/PlayerRepositoryInterface.php
+++ src/Player/Repository/PlayerRepositoryInterface.php
@@ -6,9 +6,26 @@ namespace Raid\Player\Repository;
 
 use Raid\Player\Model\Player;
 
+/**
+ * Player repository interface
+ */
 interface PlayerRepositoryInterface
 {
+    /**
+     * Saves player into repository
+     *
+     * @param Player $player
+     *
+     * @return void
+     */
     public function savePlayer(Player $player): void;
 
+    /**
+     * Finds player by name
+     *
+     * @param string $playerName
+     *
+     * @return Player|null
+     */
     public function findPlayerByName(string $playerName): ?Player;
 }
