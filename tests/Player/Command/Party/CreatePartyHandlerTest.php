fb0f73924fa229e1e01be507149a4439c2ef1764
Temirkhan <diezza.com@gmail.com>
2019-04-29 19:10:12 +0300

    Party creation test has been written

---
+++ tests/Player/Command/Party/CreatePartyHandlerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Raid\Player\Command\Party;
+
+use Raid\AbstractCommandHandleTest;
+use Raid\Character\ValueObject\CharacterPreset;
+use Raid\Player\Model\Player;
+
+/**
+ * Party creation tests
+ */
+class CreatePartyHandlerTest extends AbstractCommandHandleTest
+{
+    /**
+     * Tests party creation
+     *
+     * @return void
+     */
+    public function testCreateParty(): void
+    {
+        $partyLeader = $this->createPlayer('PartyLeader');
+        $this->assertNull($partyLeader->getParty());
+        $somePlayer = $this->createPlayer('SomePlayer');
+
+        $command = new CreateParty('PartyLeader', 'SomePlayer');
+        $this->runCommand($command);
+
+        $party = $partyLeader->getParty();
+        $this->assertNotNull($party);
+        $this->assertTrue($party->hasPlayer($somePlayer));
+    }
+
+    /**
+     * Creates player
+     *
+     * @param string $playerName
+     *
+     * @return Player
+     */
+    private function createPlayer(string $playerName): Player
+    {
+        $playerPreset = new CharacterPreset($playerName, 123, 321, 100);
+        $player       = new Player($playerPreset);
+
+        $playerRepository = $this->getService('raid.player.repository.player');
+
+        $playerRepository->savePlayer($player);
+
+        return $player;
+    }
+}
