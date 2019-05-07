c971165bf8738070d9827f939a73ab4e12353a37
Temirkhan <diezza.com@gmail.com>
2019-05-07 18:06:30 +0300

    Raid model created

---
+++ src/Boss/Model/Raid.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Raid\Boss\Model;
+
+use Raid\Player\Model\Party\Party;
+
+/**
+ * Raid
+ */
+class Raid
+{
+    /**
+     * Boss
+     *
+     * @var BossInterface
+     */
+    private $boss;
+
+    /**
+     * Party
+     *
+     * @var Party
+     */
+    private $party;
+
+    /**
+     * Constructor
+     *
+     * @param BossInterface $boss
+     * @param Party         $party
+     */
+    public function __construct(BossInterface $boss, Party $party)
+    {
+        $this->boss  = $boss;
+        $this->party = $party;
+    }
+
+    /**
+     * Returns boss
+     *
+     * @return BossInterface
+     */
+    public function getBoss(): BossInterface
+    {
+        return $this->boss;
+    }
+
+    /**
+     * Returns party
+     *
+     * @return Party
+     */
+    public function getParty(): Party
+    {
+        return $this->party;
+    }
+}
